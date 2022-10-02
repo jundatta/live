cab0b8d906be249a659a4571d2097c517fe2a29c
jundatta <jundatta@msn.com>
2022-10-02 12:48:21 +0900

    NEORTさんに上がったので取り込み直しました。

--- sketch_221002i/sketch_221002i.js
+++ sketch_221002i/sketch_221002i.js
@@ -1,36 +1,59 @@
 // 【作者】へいへい📫１人前のプログラマ＆脱引きこもり目指す🔥
-// 【作品名】randomMoveDot
+// 【作品名】randomMoveDotRandomMoveMarvelDots
 // https://0000.studio/shoheyhey
+// 【NEORT】へいへい📬リモートで生活＆月収20万円めざす(脱引きこもりも
+// https://neort.io/art/ccsgafc3p9f4hhfti9eg
 
-t=0
-  dts=[]
-  c=["#eebb00", "#77ddaa", "#ee99dd"]
-  draw=_=> {
-  t||createCanvas(W=600, W)
-    clear()
-    d=random(TWO_PI)
-    dts.push( {
+function setup() {
+  t = 0
+    dts = []
+    colorCode = ["#eebb00", "#77ddaa", "#ee99dd"]
+    createCanvas(Width=600, Width)
+    //falsy(false,null,0,undifind...)ならcreateCanvas
+}//end setup
+
+function draw() {
+
+  clear()
+    d = random(TWO_PI)
+    dts.push( { //配列作成追加
   x:
-    random(W), y:
-    random(W), vx:
-    2*random(0.11, 1.5)*cos(d), vy:
-    2*random(0.11, 1.5)*cos(d), l:
-  100, c:
-    random(c), s:
+    random(Width),
+    y:
+    random(Width),
+    vx:
+    2*random(0.11, 1.5)*cos(d),
+    vy:
+    2*random(0.11, 1.5)*cos(d),
+    transparent:
+    100,
+    color:
+    random(colorCode),
+    size:
     random(40)
   }
   )
-  dts=dts.filter((dt)=>(dt.l>0))
+  dts = dts.filter(
+    (dt) => ( dt.transparent > 0 )　//無名関数
+    )
+
     for (dt of dts) {
-    dt.x+=dt.vx
-      dt.y+=dt.vy
-      dt.l-=1
+    dt.x += dt.vx
+      dt.y += dt.vy
+      dt.transparent -= 1
+
       push()
       noStroke()
-      o=(dt.l>9)?Math.ceil(dt.l*2.55).toString(16):'00'
-      fill(dt.c+o)
-      ellipse(dt.x, dt.y, dt.s)
-      pop()
-  }
-  t++
-}
+
+      if (dt.transparent > 9)
+      opacity = Math.ceil(dt.transparent*2.55).toString(16)
+  else if (dt.transparent <= 9)
+  opacity = '00'
+
+    fill(dt.color + opacity)
+    ellipse(dt.x, dt.y, dt.size)
+    pop()
+}//end for
+
+t++
+} //end draw
