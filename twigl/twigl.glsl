2cd62ad86e4a070cf68c2752f3ca8baf4f0db1bf
jundatta <jundatta@msn.com>
2023-01-09 12:07:20 +0900

    Kamoshikaさん（4/10）

--- twigl/twigl.glsl
+++ twigl/twigl.glsl
@@ -29,13 +29,112 @@ vec3 hsv(float h, float s, float v){
     vec3 p = abs(fract(vec3(h) + t.xyz) * 6.0 - vec3(t.w));
     return v * mix(vec3(t.x), clamp(p - vec3(t.x), 0.0, 1.0), s);
 }
+mat3 rotate3D(float angle, vec3 axis){
+    vec3 a = normalize(axis);
+    float s = sin(angle);
+    float c = cos(angle);
+    float r = 1.0 - c;
+    return mat3(
+        a.x * a.x * r + c,
+        a.y * a.x * r + a.z * s,
+        a.z * a.x * r - a.y * s,
+        a.x * a.y * r - a.z * s,
+        a.y * a.y * r + c,
+        a.z * a.y * r + a.x * s,
+        a.x * a.z * r + a.y * s,
+        a.y * a.z * r - a.x * s,
+        a.z * a.z * r + c
+    );
+}
 // fract sin noise
 float fsnoise(vec2 c){
     return fract(sin(dot(c, vec2(12.9898, 78.233))) * 43758.5453);
 }
+//--------------- snoise2D ---------------------------------------------------------------------------
+// Description : Array and textureless GLSL 2D simplex noise function.
+//      Author : Ian McEwan, Ashima Arts.
+//  Maintainer : stegu
+//     Lastmod : 20110822 (ijm)
+//     License : Copyright (C) 2011 Ashima Arts. All rights reserved.
+//               Distributed under the MIT License. See LICENSE file.
+//               https://github.com/ashima/webgl-noise
+//               https://github.com/stegu/webgl-noise
+// 
 
-#define F fsnoise(I+=I)
+vec3 mod289(vec3 x) {
+  return x - floor(x * (1.0 / 289.0)) * 289.0;
+}
+
+vec2 mod289(vec2 x) {
+  return x - floor(x * (1.0 / 289.0)) * 289.0;
+}
+
+vec3 permute(vec3 x) {
+  return mod289(((x*34.0)+1.0)*x);
+}
+
+float snoise2D(vec2 v)
+  {
+  const vec4 C = vec4(0.211324865405187,  // (3.0-sqrt(3.0))/6.0
+                      0.366025403784439,  // 0.5*(sqrt(3.0)-1.0)
+                     -0.577350269189626,  // -1.0 + 2.0 * C.x
+                      0.024390243902439); // 1.0 / 41.0
+// First corner
+  vec2 i  = floor(v + dot(v, C.yy) );
+  vec2 x0 = v -   i + dot(i, C.xx);
+
+// Other corners
+  vec2 i1;
+  //i1.x = step( x0.y, x0.x ); // x0.x > x0.y ? 1.0 : 0.0
+  //i1.y = 1.0 - i1.x;
+  i1 = (x0.x > x0.y) ? vec2(1.0, 0.0) : vec2(0.0, 1.0);
+  // x0 = x0 - 0.0 + 0.0 * C.xx ;
+  // x1 = x0 - i1 + 1.0 * C.xx ;
+  // x2 = x0 - 1.0 + 2.0 * C.xx ;
+  vec4 x12 = x0.xyxy + C.xxzz;
+  x12.xy -= i1;
+
+// Permutations
+  i = mod289(i); // Avoid truncation effects in permutation
+  vec3 p = permute( permute( i.y + vec3(0.0, i1.y, 1.0 ))
+		+ i.x + vec3(0.0, i1.x, 1.0 ));
+
+  vec3 m = max(0.5 - vec3(dot(x0,x0), dot(x12.xy,x12.xy), dot(x12.zw,x12.zw)), 0.0);
+  m = m*m ;
+  m = m*m ;
+
+// Gradients: 41 points uniformly over a line, mapped onto a diamond.
+// The ring size 17*17 = 289 is close to a multiple of 41 (41*7 = 287)
+
+  vec3 x = 2.0 * fract(p * C.www) - 1.0;
+  vec3 h = abs(x) - 0.5;
+  vec3 ox = floor(x + 0.5);
+  vec3 a0 = x - ox;
+
+// Normalise gradients implicitly by scaling m
+// Approximation of: m *= inversesqrt( a0*a0 + h*h );
+  m *= 1.79284291400159 - 0.85373472095314 * ( a0*a0 + h*h );
+
+// Compute final noise value at P
+  vec3 g;
+  g.x  = a0.x  * x0.x  + h.x  * x0.y;
+  g.yz = a0.yz * x12.xz + h.yz * x12.yw;
+  return 130.0 * dot(m, g);
+}
+//--------------- snoise2D ---------------------------------------------------------------------------
 
 void main(void) {
-vec3 P,R=normalize(vec3((FC.xy*2.-r)/r.y,2))*rotate3D(t*.2,vec3(1,9,1));P.z=.1;float i,d=1.;for(;i++<2e2&&d>1e-4;P+=R*d){vec2 v=R.xz,c=ceil(P.xz+v*1e-3)-.5;v=(c-P.xz+.5*sign(v))/v;d=min(min(v.x,v.y),P.y-snoise2D((c+t)*.1)*abs(c.x)+9.);}o+=1.-25./i;
+	vec3 P,R=normalize(vec3((FC.xy*2.-r)/r.y,2))*rotate3D(t*.2,vec3(1,9,1));
+	P = vec3(0);
+	P.z=.1;
+	float i = 0.0;
+	vec4 ooo = vec4(0);
+	for(float d=1.;i++<2e2&&d>1e-4;P+=R*d){
+		vec2 v=R.xz,c=ceil(P.xz+v*1e-3)-.5;
+		v=(c-P.xz+.5*sign(v))/v;
+		d=min(min(v.x,v.y),P.y-snoise2D((c+t)*.1)*abs(c.x)+9.);
+	}
+	ooo+=1.-25./i;
+	ooo.a = 1.0;
+	o = ooo;
 }
