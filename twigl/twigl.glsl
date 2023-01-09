6e98e22facac6364a386dc4f08ec1de5cbea4412
jundatta <jundatta@msn.com>
2023-01-09 15:59:18 +0900

    Kamoshikaさん（8/10）

--- twigl/twigl.glsl
+++ twigl/twigl.glsl
@@ -126,5 +126,18 @@ float snoise2D(vec2 v)
 #define X Q*=10.,d=max(d,(abs(dot(sin(Q),cos(Q.yzx)))
 
 void main(void) {
-vec3 P,Q;P.z=2.;float i,d=1.;for(;i++<99.&&d>1e-4;)Q=P,d=length(Q)-.8,Q*=rotate3D(t,vec3(1)),X-.03)/10.),X-.3)/100.),P+=normalize(vec3((FC.xy*2.-r)/r.y,-2.))*d*.6;o.rgb+=hsv(-length(P)+.3,.7,20./i);
+	vec3 P,Q;
+	P = vec3(0);
+	P.z=2.;
+	float i = 0.0;
+	for(float d=1.;i++<99.&&d>1e-4;) {
+		Q=P;
+		d=length(Q)-.8;
+		Q*=rotate3D(t,vec3(1)),X-.03)/10.),X-.3)/100.);
+		P+=normalize(vec3((FC.xy*2.-r)/r.y,-2.))*d*.6;
+	}
+	vec4 ooo = vec4(0);
+	ooo.rgb+=hsv(-length(P)+.3,.7,20./i);
+	ooo.a = 1.0;
+	o = ooo;
 }
