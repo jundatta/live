896abbf2f48861a5e2caba84ee9ba913112dcb32
jundatta <jundatta@msn.com>
2023-04-16 22:39:45 +0900

    spotLight()をpointLight()に変えてみた（00:00 Studio）

--- sketch_230415b/sketch_230415b.js
+++ sketch_230415b/sketch_230415b.js
@@ -1,13 +1,16 @@
 let f=0;
 setup=_=> {
-  createCanvas(W=400, W, WEBGL);
+  //createCanvas(W=400, W, WEBGL);
+  createCanvas(1600, 1000, WEBGL);
   noStroke();
 }
 draw=_=> {
   background(0);
   rotateY(1);
   //[-1, -1, 1].map(i=>spotLight(200, 200, 200, -400, 400*i, 100, 1, -i, -.2));
-  [-1, -1, 1].map(i=>spotLight(200, 200, 200, -400, 400*i, 100, 1, -i, -.2));
+  [-1, -1, 1].map(i=>pointLight(200, 200, 200, -400, 400*i, 100));
+  // 見えないので青の平行光線を置いてみました。
+  //directionalLight(100, 200, 250, 0, +1, 0);
   push();
   translate(0, 30-abs(cos(f/31.8))*200);
   sphere(30);
@@ -15,8 +18,6 @@ draw=_=> {
   for (i=-200; i<1000; i+=100) {
     push();
     translate(i-f%100-50, 200-i+f%100);
-    y = 200-i+f%100;
-    console.log(y);
     box(100, 200, 1000);
     pop();
   }
